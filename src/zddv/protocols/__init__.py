5afa1d3df9c353603e3890a0878f6478d7d21c37
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-22 00:43:56 +0300

    v0.5: export AXI4 protocol analyzer

--- src/zddv/protocols/__init__.py
+++ src/zddv/protocols/__init__.py
@@ -1,3 +1,12 @@
 from zddv.protocols.apb import analyze_apb_file, analyze_apb_trace
+from zddv.protocols.axi4 import analyze_axi4_file, analyze_axi4_trace
+from zddv.protocols.axi4lite import analyze_axi4lite_file, analyze_axi4lite_trace
 
-__all__ = ["analyze_apb_file", "analyze_apb_trace"]
+__all__ = [
+    "analyze_apb_file",
+    "analyze_apb_trace",
+    "analyze_axi4_file",
+    "analyze_axi4_trace",
+    "analyze_axi4lite_file",
+    "analyze_axi4lite_trace",
+]
