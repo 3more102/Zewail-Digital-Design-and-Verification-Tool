e990997fff9f81878fd12bec9f7be72699492fd8
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-22 00:50:01 +0300

    protocols: export UCIe trace analyzer

--- src/zddv/protocols/__init__.py
+++ src/zddv/protocols/__init__.py
@@ -1,3 +1,9 @@
 from zddv.protocols.apb import analyze_apb_file, analyze_apb_trace
+from zddv.protocols.ucie import analyze_ucie_file, analyze_ucie_trace
 
-__all__ = ["analyze_apb_file", "analyze_apb_trace"]
+__all__ = [
+    "analyze_apb_file",
+    "analyze_apb_trace",
+    "analyze_ucie_file",
+    "analyze_ucie_trace",
+]
