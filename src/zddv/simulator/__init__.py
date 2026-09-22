5556c12cecc31c801500015038f17469d4c44beb
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-22 07:37:16 +0300

    v0.6: export Questa simulator backend

--- src/zddv/simulator/__init__.py
+++ src/zddv/simulator/__init__.py
@@ -1,9 +1,11 @@
 from .base import BuildResult, RunResult, SimulatorBackend
+from .questa import QuestaBackend
 from .verilator import VerilatorBackend
 
 __all__ = [
     "BuildResult",
     "RunResult",
     "SimulatorBackend",
+    "QuestaBackend",
     "VerilatorBackend",
 ]
