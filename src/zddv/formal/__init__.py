dcbdeeea6fe12fc77c4c2611ad5a86ccbed96e9d
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-22 13:23:21 +0300

    v0.7: add formal package exports

---
+++ src/zddv/formal/__init__.py
@@ -0,0 +1,10 @@
+from zddv.formal.base import FormalBackend, FormalRunRequest, FormalRunResult
+from zddv.formal.results import analyze_formal_file, normalize_formal_data
+
+__all__ = [
+    "FormalBackend",
+    "FormalRunRequest",
+    "FormalRunResult",
+    "analyze_formal_file",
+    "normalize_formal_data",
+]
