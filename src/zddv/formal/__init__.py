ecbe47357e41c2ce4bfcbfd2214c08c3b6b1cd72
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-22 13:37:09 +0300

    v0.7: export SymbiYosys backend

--- src/zddv/formal/__init__.py
+++ src/zddv/formal/__init__.py
@@ -4,10 +4,12 @@ from .base import (
     FormalCheckResult,
     FormalPropertyResult,
 )
+from .symbiyosys import SymbiYosysBackend
 
 __all__ = [
     "FormalBackend",
     "FormalCheckRequest",
     "FormalCheckResult",
     "FormalPropertyResult",
+    "SymbiYosysBackend",
 ]
