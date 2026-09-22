5d0de7183d2a048acfcd6bdb9a029fc6f4b165ea
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-22 13:35:15 +0300

    v0.7: export SymbiYosys formal adapter

--- src/zddv/formal/__init__.py
+++ src/zddv/formal/__init__.py
@@ -4,10 +4,13 @@ from .base import (
     FormalCheckResult,
     FormalPropertyResult,
 )
+from .sby import SymbiYosysBackend, render_sby_bmc_config
 
 __all__ = [
     "FormalBackend",
     "FormalCheckRequest",
     "FormalCheckResult",
     "FormalPropertyResult",
+    "SymbiYosysBackend",
+    "render_sby_bmc_config",
 ]
