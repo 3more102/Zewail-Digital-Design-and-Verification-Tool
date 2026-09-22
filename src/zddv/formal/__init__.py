0b71cb4c005ef3f8faaa2f3c94ce6f6ee412bcba
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-22 21:57:52 +0300

    formal: export trace cross-probe API

--- src/zddv/formal/__init__.py
+++ src/zddv/formal/__init__.py
@@ -8,6 +8,7 @@ from .coverage import (
     aggregate_formal_cover_coverage,
     write_formal_cover_coverage_report,
 )
+from .crossprobe import build_formal_trace_crossprobe, write_formal_trace_crossprobe_report
 from .sby import SymbiYosysBackend, render_sby_bmc_config, render_sby_cover_config
 from .sby_results import analyze_sby_log, parse_sby_log
 from .vcd_trace import ingest_formal_vcd_trace, parse_formal_vcd_trace
@@ -20,6 +21,8 @@ __all__ = [
     "SymbiYosysBackend",
     "aggregate_formal_cover_coverage",
     "write_formal_cover_coverage_report",
+    "build_formal_trace_crossprobe",
+    "write_formal_trace_crossprobe_report",
     "render_sby_bmc_config",
     "render_sby_cover_config",
     "analyze_sby_log",
