8437fec9361b61a0d0d6b0a6e31a79f6ada4694c
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-22 13:32:02 +0300

    v0.7: export formal counterexample API

--- src/zddv/formal/__init__.py
+++ src/zddv/formal/__init__.py
@@ -4,10 +4,18 @@ from .base import (
     FormalCheckResult,
     FormalPropertyResult,
 )
+from .counterexample import (
+    COUNTEREXAMPLE_SCHEMA,
+    ingest_formal_counterexample,
+    normalize_formal_counterexample,
+)
 
 __all__ = [
+    "COUNTEREXAMPLE_SCHEMA",
     "FormalBackend",
     "FormalCheckRequest",
     "FormalCheckResult",
     "FormalPropertyResult",
+    "ingest_formal_counterexample",
+    "normalize_formal_counterexample",
 ]
