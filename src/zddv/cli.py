0d5599d82175f65bb34f3b6f5534e0e033ba3eb5
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-22 23:19:55 +0300

    fix: import signed release CLI handlers

--- src/zddv/cli.py
+++ src/zddv/cli.py
@@ -60,6 +60,7 @@ from zddv.protocols.axi4_waveform import analyze_axi4_waveform
 from zddv.protocols.ucie import analyze_ucie_file
 from zddv.regression import run_regression
 from zddv.reporting import write_junit_report
+from zddv.release import export_verification_release, verify_verification_release
 from zddv.simulator import get_backend
 from zddv.signoff import write_verification_signoff_bundle
 from zddv.storage import (
